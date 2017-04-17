47b01cbd54679040a6f14db90fd2857f6585ded2
annTsykunova <tsykunovaane4ka@gmail.com>
2017-04-17 15:20:33 +0300

    Update A.java

--- src/main/com/epam/defaultmethod/A.java
+++ src/main/com/epam/defaultmethod/A.java
@@ -4,9 +4,6 @@ import com.epam.loopcontrol.TestLoop;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-/**
- * Created by Hanna_Tsykunova on 4/17/2017.
- */
 public interface A {
     Logger LOG = LogManager.getLogger(A.class);
     static void sayHello(){LOG.info("Hello");}
