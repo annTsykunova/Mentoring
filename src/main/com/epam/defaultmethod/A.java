fec4dd7ed77af87e53a1104922c05a40b16fd88f
annTsykunova <tsykunovaane4ka@gmail.com>
2017-04-17 15:20:54 +0300

    Update A.java

--- src/main/com/epam/defaultmethod/A.java
+++ src/main/com/epam/defaultmethod/A.java
@@ -7,7 +7,5 @@ import org.apache.logging.log4j.Logger;
 public interface A {
     Logger LOG = LogManager.getLogger(A.class);
     static void sayHello(){LOG.info("Hello");}
-    default void sayHi() {
-        LOG.info("Hi");
-    }
+    default void sayHi() {LOG.info("Hi");}
 }
